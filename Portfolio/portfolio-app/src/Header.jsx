bb5293cce49e15806b28df32f93947cc83b34511
emilvestlund <emilwestlund@hotmail.com>
2026-01-03 00:42:25 +0100

    portfolio v1

---
+++ Portfolio/portfolio-app/src/Header.jsx
@@ -0,0 +1,29 @@
+
+function Header() {
+    
+    return (
+        <header className="header">
+
+            <div className="header-container">
+
+                <div className="title-block">
+                    <h1>Emil</h1>
+                    <h4>Webdeveloper & Network Engineer</h4>
+                </div>
+
+                <nav>
+                    <ul className="nav-links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Education</a></li>
+                        <li><a href="#">Experience</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </header>
+    );
+}
+
+export default Header
