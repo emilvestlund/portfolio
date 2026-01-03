30aa608b78447e798742aab5eb3486e3a85ccbd0
emilvestlund <emilwestlund@hotmail.com>
2026-01-03 01:06:10 +0100

    Revert "Delete Portfolio directory"
    This reverts commit 91b5c3eb97da66bfd368707396095f79c4348fe4.

---
+++ Portfolio/portfolio-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Content from './Content.jsx';
+import Header from './Header.jsx';
+import Logo from './Logo.jsx';
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <div className="spacer">
+    <Logo></Logo>
+    <Content></Content>
+    </div>
+    </>
+  );
+  
+}
+
+export default App
---
+++ Portfolio/portfolio-app/src/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from './assets/CLogo.png';
+
+function Logo() {
+    return (
+        <div className="logo-wrapper">
+            <img className="Logo" src={logo} alt="Logo" />
+        </div>
+    );
+}
+
+export default Logo;
